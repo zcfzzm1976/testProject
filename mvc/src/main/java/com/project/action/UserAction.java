02260619786d54d2e35b255f2f4ead138038abec
zcfzzm1976 <zcf1976zzm>
2020-03-07 20:53:41 +0800

    添加方法

--- mvc/src/main/java/com/project/action/UserAction.java
+++ mvc/src/main/java/com/project/action/UserAction.java
@@ -4,6 +4,9 @@ import com.project.bean.UserBean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 @RequestMapping("user")
 public class UserAction {
@@ -11,5 +14,12 @@ public class UserAction {
     @RequestMapping("getUser")
     public UserBean getBean(){
         return new UserBean(1,"张三","13594864390");
+    }
+
+    @RequestMapping("findAll")
+    public List<UserBean> getList(){
+        return Arrays.asList(new UserBean(1,"张三","13594864390"),
+                new UserBean(2,"李四","15298543212"),
+                new UserBean(3,"赵峰","13455621890"));
     }
 }
